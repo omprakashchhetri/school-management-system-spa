0909d25a95ed51a0d05b34f33363a2e658bebf5c
omprakashchhetri <omprakashchhetri>
2025-09-19 00:06:07 +0530

    Employee Sidebar Added

---
+++ backend/app/Views/templates/sidebar-employee.php
@@ -0,0 +1,43 @@
+<!--==================== Sidebar Overlay End ====================-->
+<div class="side-overlay"></div>
+<!--==================== Sidebar Overlay End ====================-->
+
+<!-- ============================ Sidebar Start ============================ -->
+
+<aside class="sidebar">
+    <!-- sidebar close btn -->
+    <button type="button"
+        class="sidebar-close-btn text-gray-500 hover-text-white hover-bg-main-600 text-md w-24 h-24 border border-gray-100 hover-border-main-600 d-xl-none d-flex flex-center rounded-circle position-absolute"><i
+            class="ph ph-x"></i></button>
+    <!-- sidebar close btn -->
+
+    <a href="index.html"
+        class="sidebar__logo text-center p-20 position-sticky inset-block-start-0 bg-white w-100 z-1 pb-10">
+        <img src="<?=base_url()?>assets/images/logo/logo.png" alt="Logo">
+    </a>
+    <div class="sidebar-menu-wrapper overflow-y-auto scroll-sm">
+        <div class="p-20 pt-10">
+            <ul class="sidebar-menu">
+                <li class="sidebar-menu__item">
+                    <a href="admin/dashboard" class="nav_js sidebar-menu__link">
+                        <span class="icon"><i class="ph ph-squares-four"></i></span>
+                        <span class="text">Employee Dashboard</span>
+                    </a>
+                </li>
+                <li class="sidebar-menu__item">
+                    <a href="admin/role-list" class="nav_js sidebar-menu__link">
+                        <span class="icon"><i class="ph ph-users-three"></i></span>
+                        <span class="text">Employee List</span>
+                    </a>
+                </li>
+                <li class="sidebar-menu__item">
+                    <a href="admin/class-list" class="nav_js sidebar-menu__link">
+                        <span class="icon"><i class="ph ph-list-bullets"></i></span>
+                        <span class="text">Demo Link</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</aside>
+<!-- ============================ Sidebar End  ============================ -->
