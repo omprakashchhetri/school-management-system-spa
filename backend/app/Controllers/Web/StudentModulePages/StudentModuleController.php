198c61686928add3d1bb29e70ca6905a35aa22cc
pilgrimsage <sksmukhia.dev@gmail.com>
2026-01-22 14:38:53 +0530

    Student topbar,header,footer extracted

--- backend/app/Controllers/Web/StudentModulePages/StudentModuleController.php
+++ backend/app/Controllers/Web/StudentModulePages/StudentModuleController.php
@@ -9,38 +9,38 @@ class StudentModuleController extends BaseController
 
     public function dashboard(): string
     {
-        return view('templates/header')
+        return view('templates/header-student')
             .  view('templates/sidebar-student')
-            .  view('templates/topbar')
+            .  view('templates/topbar-student')
             .  view('pages/student-module-pages/student-dashboard')
-            .  view('templates/footer')
+            .  view('templates/footer-student')
         ;
     }
 
     public function profile(): string
     {
-        return view('templates/header')
+        return view('templates/header-student')
             .  view('templates/sidebar-student')
-            .  view('templates/topbar')
+            .  view('templates/topbar-student')
             .  view('pages/student-module-pages/student-profile')
-            .  view('templates/footer')
+            .  view('templates/footer-student')
         ;
     }
    
     public function document_list(): string
     {
-        return view('templates/header')
+        return view('templates/header-student')
             .  view('templates/sidebar-student')
-            .  view('templates/topbar')
+            .  view('templates/topbar-student')
             .  view('pages/student-module-pages/document-list')
-            .  view('templates/footer')
+            .  view('templates/footer-student')
         ;
     }
 
     public function student_report() {
        
         return view('templates/sidebar-attendance')
-            .  view('templates/topbar')
+            .  view('templates/topbar-student')
             .  view('pages/student-module-pages/student-report')
         ;
     }
@@ -48,7 +48,7 @@ class StudentModuleController extends BaseController
     public function report_card() {
        
         return view('templates/sidebar-attendance')
-            .  view('templates/topbar')
+            .  view('templates/topbar-student')
             .  view('pages/student-module-pages/report-card')
         ;
     }
---
+++ backend/app/Views/templates/footer-student.php
@@ -0,0 +1,36 @@
+   <div class="dashboard-footer py-8">
+       <div class="flex-between flex-wrap gap-16">
+           <p class="text-gray-300 text-13 fw-normal"> &copy; Copyright Echocrew 2024, All Right Reserverd
+           </p>
+       </div>
+   </div>
+   </div>
+
+   <!-- Phosphor Js -->
+   <script src="<?=base_url()?>assets/js/phosphor-icon.js"></script>
+   <!-- file upload -->
+   <script src="<?=base_url()?>assets/js/file-upload.js"></script>
+   <!-- file upload -->
+   <script src="<?=base_url()?>assets/js/plyr.js"></script>
+   <!-- dataTables -->
+   <script src="https://cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
+   <!-- full calendar -->
+   <script src="<?=base_url()?>assets/js/full-calendar.js"></script>
+   <!-- jQuery UI -->
+   <script src="<?=base_url()?>assets/js/jquery-ui.js"></script>
+   <!-- jQuery UI -->
+   <script src="<?=base_url()?>assets/js/editor-quill.js"></script>
+   <!-- apex charts -->
+   <script src="<?=base_url()?>assets/js/apexcharts.min.js"></script>
+   <!-- jvectormap Js -->
+   <script src="<?=base_url()?>assets/js/jquery-jvectormap-2.0.5.min.js"></script>
+   <!-- jvectormap world Js -->
+   <script src="<?=base_url()?>assets/js/jquery-jvectormap-world-mill-en.js"></script>
+
+   <!-- main js -->
+   <script id="mainJsScript" src="<?=base_url()?>assets/js/main.js"></script>
+
+
+   </body>
+
+   </html>
